3a5ba9c6e2e026d278cda4c7b56762454d38b4c5
alxmart <alxmartinez@gmail.com>
2026-07-16 18:30:43 -0300

    Adding more exercises, using if, and,or,not

---
+++ if_not.py
@@ -0,0 +1,12 @@
+
+credits = 130
+gpa = 1.8
+
+if not (credits >= 120):
+  print("You do not have enough credits to graduate.")
+
+if not (gpa >= 2.0):
+  print("Your GPA is not high enough to graduate.")
+
+if not (credits >= 120) or not (gpa >= 2.0):
+  print("You do not meet either requirement to graduate!")
